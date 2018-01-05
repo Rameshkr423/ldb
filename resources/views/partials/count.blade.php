50933633a3dbbc7655e06276e879076e1064c31c
mohan.sg <mohan.sg@payoda.com>
2018-01-05 10:40:56 +0530

    filter design changes.

--- resources/views/partials/count.blade.php
+++ resources/views/partials/count.blade.php
@@ -1,20 +1,26 @@
 <div class="col-sm-12 total-data">
-	<div class="panel panel-default">
-		<form class="form-inline" action="/action_page.php">
-		  <div class="form-group">
-		    <label for="email">Region</label>
-			<select class="selectpicker" data-live-search="true">
-			  <option data-tokens="chennai">Chennai</option>
-			  <option data-tokens="goa">Goa</option>
-			  <option data-tokens="madurai">Madurai</option>
-			</select>
-		  </div>
-			  <div class="form-group">
-			    <label for="start-date">Aplication Start Date</label>
-			    <input placeholder="Start Date..." size="8" class="date-picker" type="text" id="start-date" name="start-date">
-			    <input placeholder="To Date..." size="8" class="date-picker" type="text" id="end-date" name="end-date">
-			  </div>
-			  <button type="submit" class="btn btn-default">Submit</button>
-		</form> 
+	<div class="row">
+		<div class="bg-white clearfix">
+			<div class="col-sm-10">
+				<div class="row data-rating">
+					<form class="form-inline" action="/action_page.php">
+					  <div class="form-group">
+					    <label for="email">Region</label>
+						<select class="selectpicker" data-live-search="true">
+						  <option data-tokens="chennai">Chennai</option>
+						  <option data-tokens="goa">Goa</option>
+						  <option data-tokens="madurai">Madurai</option>
+						</select>
+					  </div>
+						  <div class="form-group">
+						    <label for="start-date">Aplication Start Date</label>
+						    <input placeholder="Start Date..." size="8" class="date-picker" type="text" id="start-date" name="start-date">
+						    <input placeholder="To Date..." size="8" class="date-picker" type="text" id="end-date" name="end-date">
+						  </div>
+						  <button type="submit" class="btn btn-default">Submit</button>
+					</form>
+				</div>
+			</div>
+		</div>
 	</div>
 </div>
